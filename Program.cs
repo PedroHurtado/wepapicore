1f6752b64fbaa7231db75077552d27eea0a9d559
Pedro Hurtado <pedrohurt@gmail.com>
2023-05-11 12:33:55 +0200

    add ef reference

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:8081");                                            
+                          policy.AllowCredentials();
+                      });
+});
 // Add services to the container.
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,7 +34,11 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors(MyAllowSpecificOrigins);
+
 app.MapControllers();
+
+
 
 app.UseMiddleware<webapinet.Controllers.ExceptionMiddleware>();
 
